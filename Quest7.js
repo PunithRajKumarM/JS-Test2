128ea4c2eb73485df3575f055cb25c617ae51119
PunithRajKumarM <punithrajkumar.m@celestialsys.com>
2023-11-06 08:51:24 +0530

    added storage

--- Quest7.js
+++ Quest7.js
@@ -1,5 +1,5 @@
 //barChart
-const barChartDate = {
+const barChartData = {
   labels: ["A", "B", "C"],
   datasets: [
     {
@@ -24,7 +24,7 @@ const barChart = new Chart(
   document.getElementById("barChart").getContext("2d"),
   {
     type: "bar",
-    data: barChartDate,
+    data: barChartData,
   }
 );
 
@@ -50,38 +50,40 @@ const lineChart = new Chart(
 );
 
 //Histogram
+const histogramChartData = {
+  labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+  datasets: [
+    {
+      label: "Histogram",
+      data: [12, 19, 3, 5, 2, 3],
+      borderWidth: 1,
+      backgroundColor: [
+        "rgba(255,99,132,0.2)",
+        "rgba(54,162,235,0.2)",
+        "rgba(255,206,86,0.2)",
+        "rgba(75,192,192,0.2)",
+        "rgba(153,102,255,0.2)",
+        "rgba(255,159,64,0.2)",
+      ],
+      borderColor: [
+        "rgba(255,99,132,1)",
+        "rgba(54,162,235,1)",
+        "rgba(255,206,86,1)",
+        "rgba(75,192,192,1)",
+        "rgba(153,102,255,1)",
+        "rgba(255,159,64,1)",
+      ],
+      barPercentage: 1,
+      categoryPercentage: 1,
+    },
+  ],
+};
+
 const histogramChart = new Chart(
   document.getElementById("histogram").getContext("2d"),
   {
     type: "bar",
-    data: {
-      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-      datasets: [
-        {
-          label: "Histogram",
-          data: [12, 19, 3, 5, 2, 3],
-          borderWidth: 1,
-          backgroundColor: [
-            "rgba(255,99,132,0.2)",
-            "rgba(54,162,235,0.2)",
-            "rgba(255,206,86,0.2)",
-            "rgba(75,192,192,0.2)",
-            "rgba(153,102,255,0.2)",
-            "rgba(255,159,64,0.2)",
-          ],
-          borderColor: [
-            "rgba(255,99,132,1)",
-            "rgba(54,162,235,1)",
-            "rgba(255,206,86,1)",
-            "rgba(75,192,192,1)",
-            "rgba(153,102,255,1)",
-            "rgba(255,159,64,1)",
-          ],
-          barPercentage: 1,
-          categoryPercentage: 1,
-        },
-      ],
-    },
+    data: histogramChartData,
     options: {
       scales: {
         y: {
@@ -93,19 +95,19 @@ const histogramChart = new Chart(
 );
 
 //treemap
-const treemap = document.getElementById("treemap");
+const treemapData = {
+  datasets: [
+    {
+      label: "Treemap",
+      data: [12, 19, 3, 5, 2, 3],
+      borderWidth: 1,
+    },
+  ],
+};
 
-new Chart(treemap, {
+new Chart(document.getElementById("treemap"), {
   type: "treemap",
-  data: {
-    datasets: [
-      {
-        label: "Treemap",
-        data: [12, 19, 3, 5, 2, 3],
-        borderWidth: 1,
-      },
-    ],
-  },
+  data: treemapData,
   options: {
     scales: {
       y: {
@@ -117,13 +119,15 @@ new Chart(treemap, {
 
 //heatmap
 
-let heatmapData = [
+const heatmapData = [
+  [1, 20, 30],
+  [20, 1, 60],
+  [30, 60, 1],
+];
+
+let heatmap = [
   {
-    z: [
-      [1, 20, 30],
-      [20, 1, 60],
-      [30, 60, 1],
-    ],
+    z: heatmapData,
 
     type: "heatmap",
     borderColor: "black",
@@ -132,4 +136,33 @@ let heatmapData = [
 
 // let heatmap = document.getElementById("heatmap");
 
-Plotly.newPlot("heatmap", heatmapData);
+Plotly.newPlot("heatmap", heatmap);
+
+//localStorage
+localStorage.setItem("barChartData", JSON.stringify(barChartData));
+localStorage.setItem("lineChartData", JSON.stringify(lineChartData));
+localStorage.setItem("histogramChartData", JSON.stringify(histogramChartData));
+localStorage.setItem("treemapData", JSON.stringify(treemapData));
+localStorage.setItem("heatmapData", JSON.stringify(heatmapData));
+
+const storedBarChartData = JSON.parse(localStorage.getItem("barChartData")) || {
+  labels: [],
+  datasets: [],
+};
+
+const storedLineChartData = JSON.parse(
+  localStorage.getItem("lineChartData")
+) || {
+  labels: [],
+  datasets: [],
+};
+const storedHistogramChartData = JSON.parse(
+  localStorage.getItem("histogramChartData")
+) || {
+  labels: [],
+  datasets: [],
+};
+const storedTreemapData = JSON.parse(localStorage.getItem("treemapData")) || {
+  datasets: [],
+};
+const storedHeatmapData = JSON.parse(localStorage.getItem("heatmapData")) || [];
