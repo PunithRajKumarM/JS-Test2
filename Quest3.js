269c29e4c533a0a600f2f5b1e00548a174ecb10e
PunithRajKumarM <146944110+PunithRajKumarM@users.noreply.github.com>
2024-03-13 16:03:19 +0530

    Update Quest3.js

--- Quest3.js
+++ Quest3.js
@@ -1,3 +1,6 @@
+// Make a function that returns the SHA-256 secure hash for a given string. The hash should be formatted in a hexadecimal digit string.
+// Hash algorithms are easy to do one way, but essentially impossible to do in reverse. For example, if you hash something simple, like password123, it will give you a long code, unique to that word or phrase. Ideally, there's no way to do this in reverse. You can't take the hash code and go back to the word or phrase you started with.
+
 const crypto = require("crypto");
 
 function sha256Hash(str) {
