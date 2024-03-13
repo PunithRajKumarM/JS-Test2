6e18b668f471bd9dc20553c374000088053f3756
PunithRajKumarM <146944110+PunithRajKumarM@users.noreply.github.com>
2024-03-13 15:45:05 +0530

    Update Quest2.js

--- Quest2.js
+++ Quest2.js
@@ -1,3 +1,10 @@
+// Create a function that is a Hashtag Generator by using the following rules:
+
+// The output must start with a hashtag (#).
+// Each word in the output must have its first letter capitalized.
+// If the final result, a single string, is longer than 140 characters, the function should return false.
+// If either the input (str) or the result is an empty string, the function should return false.
+
 function generateHashTag(str) {
   let hashTag = "#";
   str = str
